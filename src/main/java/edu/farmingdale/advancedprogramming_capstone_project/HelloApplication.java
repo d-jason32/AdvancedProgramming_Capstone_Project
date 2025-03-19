131fd9bbc064446f9d825ef91354aeed866dcec4
Jason Devaraj <jdevaraj01@gmail.com>
2025-03-19 16:47:36 -0400

    Add splash page.

--- src/main/java/edu/farmingdale/advancedprogramming_capstone_project/HelloApplication.java
+++ src/main/java/edu/farmingdale/advancedprogramming_capstone_project/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("splash-screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 900);
+        stage.setTitle("Zoom");
         stage.setScene(scene);
         stage.show();
     }
