5c6996263e9cc60718df6595c7168afc329acfe4
Jason Devaraj <jdevaraj01@gmail.com>
2025-03-21 16:15:25 -0400

    Edit.

--- src/main/java/edu/farmingdale/advancedprogramming_capstone_project/MainApplication.java
+++ src/main/java/edu/farmingdale/advancedprogramming_capstone_project/MainApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("splash-screen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("workspace.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 900);
         stage.setTitle("Zoom");
         stage.setScene(scene);
