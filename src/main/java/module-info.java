731fc863352055f8bb828e14bd239777c93bd8f6
Yohangel Adames <adamy@farmingdale.edu>
2025-03-25 15:18:52 -0400

    Splash Scene
    Splash Scene (loading screen)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module edu.farmingdale.advancedprogramming_capstone_project {
-    requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.web;
+    requires java.sql;
+    requires client.sdk;
+    requires java.net.http;
+    requires com.azure.communication.identity;
+    requires java.desktop;
 
     opens edu.farmingdale.advancedprogramming_capstone_project to javafx.fxml;
     exports edu.farmingdale.advancedprogramming_capstone_project;
