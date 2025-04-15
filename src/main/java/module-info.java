304b942f71ef004eff9f5586fe103e64b547e7b9
Milton-Moses <exploder47z@gmail.com>
2025-04-15 15:57:43 -0400

    CSS Finished

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module edu.farmingdale.advancedprogramming_capstone_project {
     requires java.net.http;
     requires client.sdk;
     requires java.desktop;
+    requires com.microsoft.aad.msal4j;
+    requires jdk.httpserver;
 
     opens edu.farmingdale.advancedprogramming_capstone_project to javafx.fxml;
     exports edu.farmingdale.advancedprogramming_capstone_project;
